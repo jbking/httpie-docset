86c6d65f63e9dd9f1f9240d0e2fd3d4c74707f26
MURAOKA Yusuke <yusuke@jbking.org>
2018-09-27 21:17:47 +0900

    use dashing

--- gen.sh
+++ gen.sh
@@ -1,24 +1,6 @@
 #!/bin/bash -xe
 
-mkdir -p HTTPie.docset/Contents/Resources/Documents/
-
-rst2html5.py README.rst > HTTPie.docset/Contents/Resources/Documents/README.html
-
-cat <<EOF > HTTPie.docset/Contents/Info.plist
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  <key>CFBundleIdentifier</key>
-  <string>HTTPie</string>
-  <key>CFBundleName</key>
-  <string>HTTPie</string>
-  <key>DocSetPlatformFamily</key>
-  <string>httpie</string>
-  <key>isDashDocset</key>
-  <true/>
-  <key>isJavaScriptEnabled</key>
-  <false/>
-</dict>
-</plist>
-EOF
+cd $(dirname $0)
+mkdir -p build
+rst2html5.py README.rst > build/README.html
+dashing build -s build/
